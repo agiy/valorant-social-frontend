a7e3ea0cc2b66a0814712d25f4fbcba82a93a32e
agiy <agiyagiyagiy@gmail.com>
2020-06-04 22:45:10 +0900

    [update] typescript-eslintインストール

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,13 +8,14 @@ module.exports = {
     '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+    // ESLint: Parsing error: '}' expected. のエラーが解消できない
+    // 'plugin:@typescript-eslint/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier', '@typescript-eslint', 'vue'],
   // add your custom rules here
-  rules: {
-  }
+  rules: {}
 }
